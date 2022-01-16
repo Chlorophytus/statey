06e2e5f384d24dba28301855fd16092fa696713c
Roland Metivier <metivier.roland@chlorophyt.us>
2022-01-16 06:35:40 -0600

    make state variable lib

--- include/statey.hpp
+++ include/statey.hpp
@@ -1,13 +1,55 @@
 #pragma once
-#include <cmath>
 #include <cstdint>
-#include <cstdio>
-#include <memory>
-#include <optional>
-#include <queue>
-#include <rtaudio/RtAudio.h>
-#include <rtmidi/RtMidi.h>
-#include <thread>
+#include <filesystem>
+#include <future>
+#include <math.h>
+#include <numbers>
+#include <stack>
+#include <utility>
 #include <vector>
+#include <x86intrin.h>
+/// A thread safe 128-bit SIMD sound middleware
+namespace statey {
+using U8 = std::uint8_t;
+using U16 = std::uint16_t;
+using U32 = std::uint32_t;
+using U64 = std::uint64_t;
 
-constexpr float SAMPLE_RATE = 96000.0f;
+using S8 = std::int8_t;
+using S16 = std::int16_t;
+using S32 = std::int32_t;
+using S64 = std::int64_t;
+
+using F32 = float;
+using F64 = double;
+using F32x4 = F32 __attribute__((vector_size(16)));
+
+enum class role : U8 {
+  lpf,
+  hpf,
+  echo,
+  gen,
+};
+class block {
+  std::vector<F32x4> _storage;
+
+public:
+  const role type;
+  void render(F32x4 &, F32x4 &, std::vector<F32x4> &&, F32x4);
+  explicit block(const role);
+};
+
+struct instrument {
+  std::stack<block> blocks{};
+};
+
+class context {
+  std::vector<instrument> _instruments{};
+
+public:
+  using chunk = std::vector<F32>;
+
+  explicit context(std::filesystem::path &&); /// loads a notes dump
+  std::future<chunk> render(const U32);       /// renders sound data in stereo
+};
+} // namespace statey
--- src/statey.cpp
+++ src/statey.cpp
@@ -1,185 +1,115 @@
 #include "statey.hpp"
-#include "statey_audio.hpp"
-#include "statey_midi.hpp"
-int main(int argc, char const *argv[]) {
-  auto audio = std::make_unique<statey::audio>();
-  auto midi = std::make_unique<statey::midi>(1);
-  for (;;) {
-    if (!midi->queue.empty()) {
-      auto mesg = midi->queue.front();
-      switch (mesg.code) {
-      case 0x80: {
-        audio->step_off();
-        break;
-      }
-      case 0x90: {
-        audio->step_on(mesg.data0.value(), mesg.data1.value() / 127.0f);
-        break;
-      }
-      case 0xB0: {
-        auto control_command = mesg.data0.value();
-        switch (control_command) {
-        case 1: {
-          auto setting = std::pow(mesg.data1.value() / 127.0f, 2.0f) / 1024.0f;
-          for (auto &&voice : audio->voices) {
-            voice->osc0.vibrato_freqattenuate = setting;
-            voice->osc1.vibrato_freqattenuate = setting;
-          }
-          std::fprintf(stderr, "set vib amp to %f\n", setting);
-          break;
-        }
-        case 21: {
-          auto setting = std::pow(mesg.data1.value() / 127.0f, 2.0f) *
-                         (SAMPLE_RATE / 8.0f);
-          for (auto &&voice : audio->voices) {
-            voice->svf0.frequency = setting;
-          }
-          std::fprintf(stderr, "set Fc Hi to %f\n", setting);
-          break;
-        }
-        case 22: {
-          auto setting =
-              0.5f + (std::pow(mesg.data1.value() / 127.0f, 2.0f) * 99.5f);
-          for (auto &&voice : audio->voices) {
-            voice->svf0.q_factor = setting;
-          }
-          std::fprintf(stderr, "set Q Hi to %f\n", setting);
-          break;
-        }
-        case 23: {
-          auto setting = std::pow(mesg.data1.value() / 127.0f, 2.0f) *
-                         (SAMPLE_RATE / 8.0f);
-          for (auto &&voice : audio->voices) {
-            voice->svf1.frequency = setting;
-          }
-          std::fprintf(stderr, "set Fc Lo to %f\n", setting);
-          break;
-        }
-        case 24: {
-          auto setting =
-              0.5f + (std::pow(mesg.data1.value() / 127.0f, 2.0f) * 99.5f);
-          for (auto &&voice : audio->voices) {
-            voice->svf1.q_factor = setting;
-          }
-          std::fprintf(stderr, "set Q Lo to %f\n", setting);
-          break;
-        }
-        case 25: {
-          auto setting = mesg.data1.value() / 4096.0f;
-          for (auto &&voice : audio->voices) {
-            voice->osc1.frequency_multi2fine = setting;
-          }
-          std::fprintf(stderr, "set V1detune fine to %f\n", setting);
-          break;
-        }
-        case 44: {
-          auto setting = std::pow(mesg.data1.value() / 127.0f, 4.0f);
-          for (auto &&voice : audio->voices) {
-            voice->amp0.attack_rate = setting;
-          }
-          std::fprintf(stderr, "set initial to %f\n", setting);
-          break;
-        }
-        case 45: {
-          auto setting = std::pow(mesg.data1.value() / 127.0f, 4.0f);
-          for (auto &&voice : audio->voices) {
-            voice->amp0.release_rate = setting;
-          }
-          std::fprintf(stderr, "set release to %f\n", setting);
-          break;
-        }
-        case 41: {
-          auto setting = mesg.data1.value() / 127.0f;
-          for (auto &&voice : audio->voices) {
-            voice->osc0.morph = setting;
-          }
-          std::fprintf(stderr, "set V0morph to %f\n", setting);
-          break;
-        }
-        case 42: {
-          auto setting = mesg.data1.value() / 127.0f;
-          for (auto &&voice : audio->voices) {
-            voice->osc1.morph = setting;
-          }
-          std::fprintf(stderr, "set V1morph to %f\n", setting);
-          break;
-        }
-        case 43: {
-          auto rawsetting =
-              static_cast<uint8_t>(mesg.data1.value() / 127.0f * 4.0f);
-          auto setting = "what";
-          auto realsetting = 1.0f;
-          switch (rawsetting) {
-          case 0: {
-            setting = "16'";
-            realsetting = 0.25f;
-            break;
-          }
-          case 1: {
-            setting = "8'";
-            realsetting = 0.5f;
-            break;
-          }
-          case 2: {
-            setting = "4'";
-            realsetting = 1.0f;
-            break;
-          }
-          case 3: {
-            setting = "2'";
-            realsetting = 2.0f;
-            break;
-          }
-          case 4: {
-            setting = "1 1/2'";
-            realsetting = 3.0f;
-            break;
-          }
-          }
 
-          for (auto &&voice : audio->voices) {
-            voice->osc1.frequency_multi2coarse = realsetting;
-          }
-          std::fprintf(stderr, "set V1detune coarse to %s\n", setting);
-          break;
-        }
-        default:
-          break;
-        }
-        break;
-      }
-      case 0xD0: {
-        auto setting = std::pow(mesg.data0.value() / 127.0f, 2.0f) / 16.0f;
-        for (auto &&voice : audio->voices) {
-          voice->osc0.vibrato_amplitude = setting;
-          voice->osc1.vibrato_amplitude = setting;
-        }
-        std::fprintf(stderr, "channel aftertouch %f\n", setting);
-        break;
-      }
-      case 0xE0: {
-        auto setting = (mesg.data1.value() / 127.0f) + 0.5f;
-        for (auto &&voice : audio->voices) {
-          voice->osc0.frequency_multi = setting;
-          voice->osc1.frequency_multi = setting;
-        }
-        break;
-      }
-      default: {
-        std::fprintf(stderr, "DM:%2hhX ", mesg.code);
-        if (mesg.data0.has_value()) {
-          std::fprintf(stderr, "D0:%2hhX ", mesg.data0.value());
-        }
-        if (mesg.data1.has_value()) {
-          std::fprintf(stderr, "D1:%2hhX ", mesg.data1.value());
-        }
-        std::fprintf(stderr, "\n");
-        break;
-      }
-      }
-      midi->queue.pop();
+using namespace statey;
+
+constexpr static auto VEC_PI =
+    F32x4{std::numbers::pi_v<F32>, std::numbers::pi_v<F32>,
+          std::numbers::pi_v<F32>, std::numbers::pi_v<F32>};
+constexpr static auto VEC_ONE_P = F32x4{1.0f, 1.0f, 1.0f, 1.0f};
+constexpr static auto VEC_ONE_N = -VEC_ONE_P;
+constexpr static auto VEC_TWO = F32x4{2.0f, 2.0f, 2.0f, 2.0f};
+constexpr static auto VEC_TWOPI = VEC_TWO * VEC_PI;
+// const static auto VEC_HUGEVAL = _mm_set1_ps(999999.0f);
+
+F32x4 F32x4_clampones(F32x4 what) {
+  return _mm_max_ps(_mm_min_ps(what, VEC_ONE_P), VEC_ONE_N);
+}
+
+block::block(const role _type) : type{_type} {}
+
+void block::render(F32x4 &L, F32x4 &R, std::vector<F32x4> &&params,
+                   F32x4 rate) {
+  switch (type) {
+  case role::lpf:
+  case role::hpf: {
+    // delays
+    constexpr auto delayL0 = 0;
+    constexpr auto delayL1 = 1;
+    constexpr auto delayR0 = 2;
+    constexpr auto delayR1 = 3;
+    // low pass
+    constexpr auto loL = 4;
+    constexpr auto loR = 5;
+    // high pass
+    constexpr auto hiL = 6;
+    constexpr auto hiR = 7;
+    // band pass
+    constexpr auto bpL = 8;
+    constexpr auto bpR = 9;
+    // band reject
+    constexpr auto brL = 10;
+    constexpr auto brR = 11;
+
+    // PRECALCULATED by the Python 3 scripts because State Variable Filters use
+    // trig that is not supported in x86 SSE SIMD nor AVX, shame...
+    const auto PC_f_coeff = params.at(0);
+    const auto PC_q_coeff = params.at(1);
+
+    // =========================================================================
+    // left channel
+    // =========================================================================
+    _storage.at(hiL) = F32x4_clampones(
+        (((_storage.at(loL) + (_storage.at(delayL0) * PC_q_coeff)) + L)));
+    _storage.at(bpL) = F32x4_clampones(
+        ((_storage.at(hiL) * PC_f_coeff) + _storage.at(delayL0)));
+    _storage.at(loL) = F32x4_clampones(
+        ((_storage.at(delayL0) * PC_f_coeff) + _storage.at(delayL1)));
+    _storage.at(brL) = F32x4_clampones((_storage.at(hiL) + _storage.at(loL)));
+    _storage.at(delayL0) = _storage.at(bpL);
+    _storage.at(delayL1) = _storage.at(loL);
+    // =========================================================================
+    // right channel
+    // =========================================================================
+    _storage.at(hiL) = F32x4_clampones(
+        (((_storage.at(loL) + (_storage.at(delayL0) * PC_q_coeff)) + L)));
+    _storage.at(bpL) = F32x4_clampones(
+        ((_storage.at(hiL) * PC_f_coeff) + _storage.at(delayL0)));
+    _storage.at(loL) = F32x4_clampones(
+        ((_storage.at(delayL0) * PC_f_coeff) + _storage.at(delayL1)));
+    _storage.at(brL) = F32x4_clampones((_storage.at(hiL) + _storage.at(loL)));
+    _storage.at(delayL0) = _storage.at(bpL);
+    _storage.at(delayL1) = _storage.at(loL);
+
+    switch (type) {
+    case role::lpf: {
+      L = _storage.at(loL);
+      R = _storage.at(loR);
+      break;
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    case role::hpf: {
+      L = _storage.at(hiL);
+      R = _storage.at(hiR);
+      break;
+    }
+    default: {
+      break;
+    }
+    }
+
+    break;
   }
-  return 0;
+  case role::echo: {
+
+    break;
+  }
+  case role::gen: {
+    constexpr auto phase = 0;
+    const auto morph = params.at(1);
+    _storage.at(phase) = _storage.at(phase) + params.at(0);
+    // const auto subtrahend =
+    //     _mm_floor_ps(_mm_div_ps(_storage.at(phase), VEC_TWOPI));
+    // _storage.at(phase) = _mm_div_ps(
+    //     _mm_sub_ps(_storage.at(phase), _mm_add_ps(subtrahend, VEC_PI)),
+    //     rate);
+    const auto mix_saw = (_storage.at(phase) * (VEC_ONE_P - morph));
+    // const auto clip_coeff = ((params.at(2) * VEC_HUGEVAL) - VEC_HUGEVAL);
+    // const auto mix_pwm = (
+    //     _mm_clampones_ps(_mm_sub_ps(_mm_mul_ps(_storage.at(phase),
+    //     VEC_HUGEVAL), clip_coeff)) * morph);
+    break;
+  }
+  default: {
+    break;
+  }
+  }
 }
